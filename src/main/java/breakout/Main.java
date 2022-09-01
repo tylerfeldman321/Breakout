fd8c61a2e8998044813bb32129b4e2ab9a5ce45a
tyler.feldman <tyler.feldman@duke.edu>
2022-09-01 19:57:37 -0400

    Set up game with a player; add player movement

--- src/main/java/breakout/Main.java
+++ src/main/java/breakout/Main.java
@@ -4,41 +4,43 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Point2D;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
 import javafx.util.Duration;
 
 
 /**
- * Feel free to completely change this code or delete it entirely.
- *
  * @author Tyler Feldman
+ * Game setup code taken from example_animation / Robert Duvall
  */
-public class Main extends Application {
-    // useful names for constant values used
-    public static final String TITLE = "Breakout";
-    public static final int SIZE = 400;
-    public static final String RESOURCE_PATH = "/breakout/";
-    public static final String BALL_IMAGE = RESOURCE_PATH + "ball.gif";
+public class Main extends Application
+{
     public static final float ballSpeed = 10;
     public static final int FRAMES_PER_SECOND = 60;
     public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+    public static final int SIZE = 400;
+    public static final String TITLE = "Breakout";
     public static final Paint BACKGROUND = Color.AZURE;
+    public static final int PADDLE_WIDTH = 40;
+    public static final int PADDLE_HEIGHT = 5;
 
     private Scene myScene;
+    private Group myRootNode;
+    private SpriteManager spriteManager;
+    private Player myPlayer;
 
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
 
-    /**
-     * Initialize what will be displayed. Code from example_animation / Robert Duvall
-     */
     @Override
-    public void start (Stage stage) {
+    public void start(Stage stage)
+    {
         myScene = setupGame(SIZE, SIZE, BACKGROUND);
         stage.setScene(myScene);
         stage.setTitle(TITLE);
@@ -51,20 +53,35 @@ public class Main extends Application {
     }
 
     public Scene setupGame(int width, int height, Paint background) {
-        Group root = new Group();
-        myScene = new Scene(root, width, height, background);
+        spriteManager = new SpriteManager();
+
+        myRootNode = new Group();
+
+        myScene = new Scene(myRootNode, width, height, background);
+        myScene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
+
+        Point2D paddleStartingPosition = new Point2D(myScene.getWidth()/2, myScene.getHeight()/2);
+        myPlayer = new Player(PADDLE_WIDTH, PADDLE_HEIGHT, paddleStartingPosition, Color.BLACK, 10);
+        addSprite(myPlayer);
+
         return myScene;
     }
 
-    private void step(double elapsedTime) {
-
+    public void addSprite(Sprite sprite) {
+        spriteManager.addSprite(sprite);
+        myRootNode.getChildren().add(sprite.getShape());
     }
 
+    private void step(double elapsedTime) {
+        spriteManager.updateSprites(elapsedTime);
+    }
 
-    /**
-     * Start the program.
-     */
-    public static void main (String[] args) {
-        launch(args);
+    private void handleKeyInput (KeyCode code) {
+        if (code == KeyCode.RIGHT) {
+            myPlayer.moveRight();
+        }
+        else if (code == KeyCode.LEFT) {
+            myPlayer.moveLeft();
+        }
     }
 }
