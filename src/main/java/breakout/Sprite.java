0ab7730f4f30855081785fed563524799f3efcb8
tyler.feldman <tyler.feldman@duke.edu>
2022-09-01 19:39:00 -0400

    Create new constructor and complete setPosition function

--- src/main/java/breakout/Sprite.java
+++ src/main/java/breakout/Sprite.java
@@ -1,12 +1,9 @@
 package breakout;
 
 import javafx.geometry.Point2D;
-import javafx.scene.Node;
 import javafx.scene.shape.Shape;
 
-import java.awt.*;
-
-// https://gist.github.com/jewelsea/910dae32ef6d1bd4257850031aefcba2#file-breakoutgame-java
+// https://gamedevelopment.tutsplus.com/tutorials/introduction-to-javafx-for-game-development--cms-23835
 
 public class Sprite
 {
@@ -17,14 +14,21 @@ public class Sprite
     public Sprite(Shape shape, Point2D position, Point2D velocity)
     {
         this.shape = shape;
-        this.position = position;
+        setPosition(position);
         this.velocity = velocity;
+    }
+
+    public Sprite(Shape shape) {
+        this.shape = shape;
+        this.position = new Point2D(0, 0);
+        this.velocity = new Point2D(0, 0);
     }
 
     public void setPosition(Point2D position)
     {
         this.position = position;
-        this.shape.
+        this.shape.setTranslateX(position.getX());
+        this.shape.setTranslateY(position.getY());
     }
 
     public void setVelocity(Point2D velocity)
@@ -35,18 +39,11 @@ public class Sprite
     public void update(double time)
     {
         Point2D displacement = new Point2D(velocity.getX() * time, velocity.getY() * time);
-        position = position.add(displacement);
+        setPosition(position.add(displacement));
     }
-
 
     public Shape getShape()
     {
         return shape;
     }
-
-//    public boolean intersects(Sprite s)
-//    {
-//        return s.getShape().intersects( this.getShape() );
-//    }
-
 }
