c37d5e979faf1ce90e0c0a8c19b9d25e3c5495e3
tyler.feldman <tyler.feldman@duke.edu>
2022-09-02 10:52:45 -0400

    Bounds -> BoundingBox

--- src/main/java/breakout/Projectile.java
+++ src/main/java/breakout/Projectile.java
@@ -1,10 +1,12 @@
 package breakout;
 
+import javafx.geometry.BoundingBox;
+import javafx.geometry.Bounds;
 import javafx.geometry.Point2D;
 import javafx.scene.shape.Shape;
 
 public class Projectile extends Sprite {
-    public Projectile(Shape shape, Point2D position, Point2D velocity) {
-        super(shape, position, velocity);
+    public Projectile(Shape shape, BoundingBox boundingBox, Point2D position, Point2D velocity) {
+        super(shape, boundingBox, position, velocity);
     }
 }
