cbb86edac61a0e7b759207c6b898906faeae51c2
tyler.feldman <tyler.feldman@duke.edu>
2022-09-02 10:11:19 -0400

    Add bounce functions for X and Y directions

--- src/main/java/breakout/Ball.java
+++ src/main/java/breakout/Ball.java
@@ -8,4 +8,14 @@ public class Ball extends Sprite {
     public Ball(double radius, Point2D position, Color color) {
         super(new Circle(radius, color), position, new Point2D(0, 0));
     }
+
+    public void bounceX() {
+        Point2D newVelocity = new Point2D(-this.getVelocity().getX(), this.getVelocity().getY());
+        this.setVelocity(newVelocity);
+    }
+
+    public void bounceY() {
+        Point2D newVelocity = new Point2D(this.getVelocity().getX(), -this.getVelocity().getY());
+        this.setVelocity(newVelocity);
+    }
 }
