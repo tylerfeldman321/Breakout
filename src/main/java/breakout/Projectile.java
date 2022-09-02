e04f56561e9966108ecf4d7b968c77db9d61613d
tyler.feldman <tyler.feldman@duke.edu>
2022-09-02 10:15:35 -0400

    Create Projectile class

---
+++ src/main/java/breakout/Projectile.java
@@ -0,0 +1,10 @@
+package breakout;
+
+import javafx.geometry.Point2D;
+import javafx.scene.shape.Shape;
+
+public class Projectile extends Sprite {
+    public Projectile(Shape shape, Point2D position, Point2D velocity) {
+        super(shape, position, velocity);
+    }
+}
