e563128ea5f4c26c875b73238fc4bfe52998c78f
tyler.feldman <tyler.feldman@duke.edu>
2022-09-02 12:09:51 -0400

    Add velocity to constructor

--- src/main/java/breakout/Ball.java
+++ src/main/java/breakout/Ball.java
@@ -6,9 +6,9 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
 public class Ball extends Projectile {
-    public Ball(double radius, Point2D position, Color color) {
+    public Ball(double radius, Point2D position, Point2D velocity, Color color) {
         super(new Circle(radius, color),
                 position,
-                new Point2D(0, 0));
+                velocity);
     }
 }
