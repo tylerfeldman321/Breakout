949491005abb9daa2421ad186bba92bf936346a7
tyler.feldman <tyler.feldman@duke.edu>
2022-09-02 10:16:08 -0400

    Make Ball extend Projectile isntead of Sprite

--- src/main/java/breakout/Ball.java
+++ src/main/java/breakout/Ball.java
@@ -4,7 +4,7 @@ import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-public class Ball extends Sprite {
+public class Ball extends Projectile {
     public Ball(double radius, Point2D position, Color color) {
         super(new Circle(radius, color), position, new Point2D(0, 0));
     }
