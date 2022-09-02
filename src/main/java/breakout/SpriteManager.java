2eaa1ece63f7361e52d727fc6dde8c6c2b3808d9
tyler.feldman <tyler.feldman@duke.edu>
2022-09-02 11:36:21 -0400

    Keep track of projectiles in separate list

--- src/main/java/breakout/SpriteManager.java
+++ src/main/java/breakout/SpriteManager.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class SpriteManager {
     private List<Sprite> sprites = new ArrayList<>();
+    private List<Projectile> projectiles = new ArrayList<>();
 
     public void updateSprites(double elapsedTime) {
         for (Sprite sprite : sprites) {
@@ -14,6 +15,9 @@ public class SpriteManager {
 
     public void addSprite(Sprite sprite) {
         sprites.add(sprite);
+        if (sprite instanceof Projectile) {
+            projectiles.add((Projectile) sprite);
+        }
     }
 
     public void addSprites(List<Sprite> sprites) {
