bef4dabf45bf52117542acf7de50176991be8611
tyler.feldman <tyler.feldman@duke.edu>
2022-09-02 10:03:40 -0400

    Add Block class

---
+++ src/main/java/breakout/Block.java
@@ -0,0 +1,11 @@
+package breakout;
+
+import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Block extends Sprite {
+    public Block(int width, int height, Point2D position, Color color) {
+        super(new Rectangle(width, height, color), position, new Point2D(0, 0));
+    }
+}
