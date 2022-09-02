39164f8a2616a01ca6264f88624ac0bc84cf8b89
tyler.feldman <tyler.feldman@duke.edu>
2022-09-01 22:09:01 -0400

    Add rounded corners to Paddle

--- src/main/java/breakout/Paddle.java
+++ src/main/java/breakout/Paddle.java
@@ -8,5 +8,12 @@ import javafx.scene.shape.Rectangle;
 public class Paddle extends Sprite {
     public Paddle(int width, int height, Point2D position, Color color) {
         super(new Rectangle(width, height, color), position, new Point2D(0, 0));
+        setRoundedPaddleCorners();
+    }
+
+    private void setRoundedPaddleCorners() {
+        Rectangle paddleRect = (Rectangle)this.getShape();
+        paddleRect.setArcWidth(5);
+        paddleRect.setArcHeight(5);
     }
 }
