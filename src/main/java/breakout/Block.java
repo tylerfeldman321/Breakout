c70384e9fbdccccb46461f363fe114eada7102ab
tyler.feldman <tyler.feldman@duke.edu>
2022-09-03 01:46:21 -0400

    Add border to blocks

--- src/main/java/breakout/Block.java
+++ src/main/java/breakout/Block.java
@@ -3,12 +3,15 @@ package breakout;
 import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
 
 public class Block extends Sprite {
     public Block(double width, double height, Point2D position, Color color) {
         super(new Rectangle(width, height, color),
                 position,
                 new Point2D(0, 0));
+        this.getShape().setStrokeType(StrokeType.INSIDE);
+        this.getShape().setStroke(Color.BLACK);
     }
 
     public void handleCollisionWith(Sprite sprite, SpriteManager spriteManager) {
